4ee339e7166cf0e889748787e935a9b564fa7b13
Giovanni <giomadalozzo@gmail.com>
2021-03-16 17:13:28 -0300

    Adicionado atributos conta

--- ExpensesWallet/Resources/Account.swift
+++ ExpensesWallet/Resources/Account.swift
@@ -8,6 +8,16 @@
 import Foundation
 
 class Account {
+    let type: String
+    let bank: String
+    let nickname: String
+    
+    init(String type, String bank, String nickname) {
+        self.type = type
+        self.bank = bank
+        self.nickname = nickname
+    }
+    
     func report() {
         
     }
