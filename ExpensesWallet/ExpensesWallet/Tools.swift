4db7d6bc7ce44adc8af173ca0f9ad0bb349864ce
guihts2205 <guihts2205@gmail.com>
2021-03-19 15:15:10 -0300

    Update Tools.swift
    Menus de ajuda

--- ExpensesWallet/ExpensesWallet/Tools.swift
+++ ExpensesWallet/ExpensesWallet/Tools.swift
@@ -15,7 +15,8 @@ struct Tools {
         \nPor favor, digite a opção que deseja:
         selecionar - Opção para selecionar uma conta bancária já existente
         criar - Opção para criar uma nova conta bancária
-        sair - Opção para sair do programa\n
+        sair - Opção para sair do programa
+        help - Opção para abrir o menu de ajuda\n
         """)
         
         let option = readLine()
@@ -35,6 +36,8 @@ struct Tools {
             listAccounts = self.registerAccount(listAccounts: listAccounts)
         case "sair":
             self.quit()
+        case "help":
+            self.helpStartMenu()
         default:
             print("Nenhuma opção identificada. \n")
             self.startMenu(listAccounts: listAccounts)
@@ -51,7 +54,8 @@ struct Tools {
         addrec gasto - Opção para adicionar gastos recorrentes
         addrec crédito - Opção para adicionar crédito recorrentes
         voltar - Opção para voltar para o menu de seleção de contas
-        sair - Opção para sair do programa\n
+        sair - Opção para sair do programa
+        help - Opção para abrir o menu de ajuda\n
         """)
         
         let option = readLine()
@@ -71,6 +75,8 @@ struct Tools {
             earnings.add(account: selectAccount)
         case "voltar":
             self.startMenu(listAccounts: listAccounts)
+        case "help":
+            self.helpAccountMenu()
         default:
             print("Nenhuma opção identificada. \n")
             self.accountMenu(selectAccount: selectAccount)
@@ -80,11 +86,26 @@ struct Tools {
     }
     
     func helpStartMenu(){
-        //info: opção utilizada para mostrar as informações da conta selecionada. Para selecioná-la, escreva "info" no terminal (sem as aspas)
+        print("""
+        \nBEM VINDO AO MENU DE AJUDA\n
+        selecionar: esta opção permite que o usuário escolha uma das contas inseridas no programa pelo usuário. Para acessar uma conta a partir desta opção, basta digitar o número da conta desejada que é informado na tela. Para selecionar esta opção, digite 'selecionar' no terminal (sem as aspas).\n
+        criar: esta opção permite ao usuário inserir uma nova conta no programa. Nesta opção, o usuário pode entrar com o tipo de conta (corrente ou poupança), o banco ao qual a conta pertence e também um apelido para a conta. Para selecioná-la, digite 'criar' no terminal (sem as aspas).\n
+        sair: esta opção encerra o programa. Para voltar a utilizar o ExpensesWallet, o usuário deve iniciar o programa novamente. Para selecionar esta opção, digite 'sair' no terminal (sem as aspas).\n
+        """)
     }
     
     func helpAccountMenu(){
-        
+        //info: opção utilizada para mostrar as informações da conta selecionada. Para selecioná-la, escreva "info" no terminal (sem as aspas)
+        print("""
+        \nBEM VINDO AO MENU DE AJUDA\n
+        info: esta opção mostra as informações da conta selecionada. Ela exibe o tipo de conta, o banco ao qual a conta pertence e o apelido da conta. Para selecioná-la, digite 'info' no terminal (sem as aspas).\n
+        extrato: esta opção mostra o extrato da conta selecionada. Ela exibe as datas das transações, mostra se foi um crédito ou um débito e mostra o valor das transações. Para selecioná-la, digite 'extrato' no terminal (sem as aspas).\n
+        addrec gasto: esta opção permite ao usuário inserir uma transação de débito. Ela permite que o usuário entre com o valor do débito e também o dia da transação. Para selecioná-la, digite 'addrec gasto' no terminal (sem as aspas).\n
+        addrec crédito: esta opção permite ao usuário inserir uma transação de crédito. Ela permite que o usuário entre com o valor do crédito e também o dia da transação. Para selecioná-la, digite 'addrec crédito' no terminal (sem as aspas).\n
+        voltar: esta opção retorna ao menu de seleção de contas e permite que o usuário mude a conta com a qual está trabalhando. Para selecioná-la, digite 'voltar' no terminal (sem as aspas).\n
+        sair: esta opção encerra o programa. Para voltar a utilizar o ExpensesWallet, o usuário deve iniciar o programa novamente. Para selecionar esta opção, digite 'sair' no terminal (sem as aspas).\n
+        help: esta opção abre este menu de ajuda com as explicações das funções do programa. Caso queira visualizar o menu de ajuda novamente, digite 'help' no terminal (sem as aspas).\n
+        """)
     }
     
     func editEntry(){
